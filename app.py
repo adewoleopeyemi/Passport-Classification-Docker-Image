e647b640315a267010f784387562c88ab72fc69b
Free-tek <adewole63@gmail.com>
2020-07-04 23:42:17 +0100

    upload to firebase works

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, flash
 import os
 from flask_assets import Environment, Bundle
 
+import firebase_admin
+from firebase_admin import credentials, db
+from google.cloud import storage
+from firebase_admin import storage
+import requests
+import pyrebase
+#from firebase import firebase
+
+import os
+
+from datetime import datetime
+
+
+
 app = Flask(__name__)
+app.secret_key = 'hbavdy3'
+
+
+
+
 
 '''
 assets = Environment(app)
@@ -30,8 +49,100 @@ def predictOneImage():
     if request.method == "POST":
         img_url = request.files["image"]
         apikey = request.form["apikey"]
-        print(img_url, apikey)
-        return ""
+        
+        UPLOAD_FOLDER = './test'
+        app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+        
+        #save image
+        path = os.path.join(app.config['UPLOAD_FOLDER'], img_url.filename)
+        img_url.save(path)
+        
+        
+        
+        
+        if not firebase_admin._apps:
+            #fetch credentials
+            cred = credentials.Certificate("service_account2.json")
+            firebase_admin.initialize_app(cred, {'databaseURL': 'https://passport-image-classification.firebaseio.com/'})
+        
+        
+        ##validate API KEY
+        ref = db.reference('ApiKey')
+        apiKeyType = ref.child(apikey).child("type").get()
+        
+        if apiKeyType == None:
+            flash('Invalid API Key')
+            return redirect(url_for("oneImagePredict"))
+        else:
+            
+            #image_url = sys.argv[1] #we pass the url as an argument
+            
+            config = {"apiKey": "AIzaSyC1ayNr3CCXRv-cejofLx1_hNmsR-o7Coo",
+                        "authDomain": "passport-image-classification.firebaseapp.com",
+                        "databaseURL": "https://passport-image-classification.firebaseio.com",
+                        "projectId": "passport-image-classification",
+                        "storageBucket": "passport-image-classification.appspot.com",
+                        "messagingSenderId": "39733434768",
+                        "appId": "1:39733434768:web:85ec9477120baa9116fb15",
+                        "measurementId": "G-FXCMDZNL6T"}
+            
+            today = datetime.now()
+            
+            
+            firebase = pyrebase.initialize_app(config)
+            storage = firebase.storage()
+            path_on_cloud = f'images/{today}.jpg'
+            storage.child(path_on_cloud).put(img_url)
+            
+            storage.
+            '''
+            from google.cloud import storage
+
+            client = storage.Client()
+            bucket = client.get_bucket('passport-image-classification')
+            blob = bucket.blob('image.png')
+            # use pillow to open and transform the file
+            image = Image.open(img_url)
+            # perform transforms
+            image.save(outfile)
+            of = open(outfile, 'rb')
+            blob.upload_from_file(of)
+            # or... (no need to use pillow if you're not transforming)
+            blob.upload_from_filename(filename=outfile)
+            
+            '''
+            
+            
+            
+            
+            
+            '''
+            bucket = storage.bucket()
+
+            image_data = requests.get(img_url).content
+            blob = bucket.blob('new_cool_image.jpg')
+            blob.upload_from_string(
+                    image_data,
+                    content_type='image/jpg'
+                )
+            print(blob.public_url)
+            '''
+
+            '''
+            
+            os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="service_account.json"
+            #firebase_instance = firebase.FirebaseApplication('https://passport-image-classification.firebaseio.com/')
+            client = storage.Client()
+            bucket = client.get_bucket('passport-image-classification')
+            # posting to firebase storage
+            imageBlob = bucket.blob("/")
+            # imagePath = [os.path.join(self.path,f) for f in os.listdir(self.path)]
+            imagePath = img_url
+            imageBlob = bucket.blob("image.png")
+            imageBlob.upload_from_filename(imagePath)
+            ''' 
+        
+            return str(apiKeyType)
 
         '''
         #Download Image
