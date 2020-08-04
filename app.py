358a2a13f6c1ea675d87c8eae3e55aee95d4b4aa
Free-tek <adewole63@gmail.com>
2020-08-04 20:22:28 +0100

    Classification of Images in Folder works

--- app.py
+++ app.py
@@ -56,6 +56,11 @@ def multipleImagePredict():
     return render_template("multiple_image_predict.html")
 
 
+@app.route("/folderImagesPredict", methods=["GET"])
+def folderImagesPredict():
+    return render_template("folder_images_predict.html")
+
+
 
 @app.route("/predictOneImage", methods=["POST", "GET"])
 def predictOneImage():
@@ -66,7 +71,7 @@ def predictOneImage():
         print(f'this is the url {img_url.filename}')
         if img_url.filename == "":
             flash('Please upload an image')
-            return redirect(url_for("oneImagePredict"))
+            return redirect(url_for("folderImagesPredict"))
         else:
         
             UPLOAD_FOLDER = './test/test'
@@ -191,7 +196,7 @@ def predictMultipleImages():
         apikey = request.form["apikey"]
 
         if not uploaded_files or not any(f for f in uploaded_files):
-            flash('Please upload an image')
+            flash('Please upload a folder')
             return redirect(url_for("multipleImagePredict"))
         else:
 
@@ -208,7 +213,9 @@ def predictMultipleImages():
             UPLOAD_FOLDER = './test/test'
             app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-            #save image to local directory for upload to firebase storage
+
+
+            #save images to local directory for upload to firebase storage
             for file in uploaded_files:
                 path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
                 file.save(path)
@@ -442,23 +449,304 @@ def predictMultipleImages():
 
                 return render_template("multipleImagesPopUp.html", data=resultDisplayed)
 
-               
-                
-                
-               
-
-
-        
-        
-    
     else:
         return "Use a POST request to access this API, check documentation at "
     
     
     
-@app.route("/predictManyImagesFolder", methods=["POST", "GET"])
-def predictManyImagesFolder(): 
-    print("")
+@app.route("/predictImagesInFolder", methods=["POST", "GET"])
+def predictImagesInFolder(): 
+
+    if request.method == "POST":
+        uploaded_files = request.files.getlist("image")
+        apikey = request.form["apikey"]
+
+        if not uploaded_files or not any(f for f in uploaded_files):
+            flash('Please upload a folder')
+            return redirect(url_for("multipleImagePredict"))
+        else:
+
+            config = {"apiKey": "AIzaSyC1ayNr3CCXRv-cejofLx1_hNmsR-o7Coo",
+                            "authDomain": "passport-image-classification.firebaseapp.com",
+                            "databaseURL": "https://passport-image-classification.firebaseio.com",
+                            "projectId": "passport-image-classification",
+                            "storageBucket": "passport-image-classification.appspot.com",
+                            "messagingSenderId": "39733434768",
+                            "appId": "1:39733434768:web:85ec9477120baa9116fb15",
+                            "measurementId": "G-FXCMDZNL6T"}
+
+
+            
+
+            UPLOAD_FOLDER = './test/test'
+            app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+            
+            
+            saveFolder(uploaded_files)
+
+            if not firebase_admin._apps:
+                #fetch credentials
+                cred = credentials.Certificate("service_account.json")
+                firebase_admin.initialize_app(cred, {'databaseURL': 'https://passport-image-classification.firebaseio.com/'})
+
+
+            ##validate API KEY
+            ref = db.reference('ApiKey')
+            apiKeyType = ref.child(apikey).child("type").get()
+
+            if apiKeyType == None:
+                flash('Invalid API Key')
+                return redirect(url_for("oneImagePredict"))
+            else:
+                #preprocess image
+
+                test_gen = ImageDataGenerator(rescale=1./255,
+                    shear_range=0.2,
+                    zoom_range=0.2,
+                    horizontal_flip=True)
+
+                test_generator = test_gen.flow_from_directory(
+                        "test/",
+                        target_size=(224, 224),
+                        shuffle = False,
+                        class_mode='categorical',
+                        batch_size=1)
+
+                #load model
+                loaded_model = load_model('model/model1-94%.h5')
+                #make prediciton
+                predict = loaded_model.predict_generator(test_generator) 
+                array  = np.array(predict)
+                result = {}
+                for i, rows in enumerate(array):
+                    _class = np.argmax(rows)
+                    result[uploaded_files[i].filename] = _class
+
+
+                today = datetime.now()
+                current_directory = os.getcwd()
+                final_directory = os.path.join(current_directory, f'result_{today}')
+                if not os.path.exists(final_directory):
+                    os.makedirs(final_directory) 
+
+                error = 0
+                currentProgress = 0
+                resultDisplayed = {}
+
+                for key, value in result.items():
+                    if value == 0:
+                        #BAD BACKGROUND
+                        if os.path.exists(f"{final_directory}/BAD_BACKGROUND") == True:
+                            try:
+                                shutil.move(f"{UPLOAD_FOLDER}/{key}" , f"{final_directory}/BAD_BACKGROUND")
+                                resultDisplayed["BAD_BACKGROUND"] += 1
+                            except:
+                                error += 1
+                        else:
+                            #make directory
+                            os.mkdir(f"{final_directory}/BAD_BACKGROUND")
+                            try:
+                                shutil.move(f"{UPLOAD_FOLDER}/{key}" , f"{final_directory}/BAD_BACKGROUND")
+                                resultDisplayed["BAD_BACKGROUND"] = 1
+                            except Exception as e:
+                                error += 1
+                            currentProgress +=1
+
+                    elif value == 1:
+                        #BLURRY
+                        if os.path.exists(f"{final_directory}/BLURRY") == True:
+                            try:
+                                shutil.move(f"{UPLOAD_FOLDER}/{key}" , f"{final_directory}/BLURRY")
+                                resultDisplayed["BLURRY"] += 1
+                            except:
+                                error += 1
+                        else:
+                            #make directory
+                            os.mkdir(f"{final_directory}/BLURRY")
+                            try:
+                                shutil.move(f"{UPLOAD_FOLDER}/{key}" , f"{final_directory}/BLURRY")
+                                resultDisplayed["BLURRY"] = 1
+                            except Exception as e:
+                                error += 1
+                            currentProgress +=1
+
+                    elif value == 2:
+                        #FACE_IN_BACKGROUND
+                        if os.path.exists(f"{final_directory}/FACE_IN_BACKGROUND") == True:
+                            try:
+                                shutil.move(f"{UPLOAD_FOLDER}/{key}" , f"{final_directory}/FACE_IN_BACKGROUND")
+                                resultDisplayed["FACE_IN_BACKGROUND"] += 1
+                            except:
+                                error += 1
+                        else:
+                            #make directory
+                            os.mkdir(f"{final_directory}/FACE_IN_BACKGROUND")
+                            try:
+                                shutil.move(f"{UPLOAD_FOLDER}/{key}" , f"{final_directory}/FACE_IN_BACKGROUND")
+                                resultDisplayed["FACE_IN_BACKGROUND"] = 1
+                            except Exception as e:
+                                error += 1
+                            currentProgress +=1
+
+                    elif value == 3:
+                        #GOOD_IMAGES
+                        if os.path.exists(f"{final_directory}/GOOD_IMAGES") == True:
+                            try:
+                                shutil.move(f"{UPLOAD_FOLDER}/{key}" , f"{final_directory}/GOOD_IMAGES")
+                                resultDisplayed["GOOD_IMAGES"] += 1
+                            except:
+                                error += 1
+                        else:
+                            #make directory
+                            os.mkdir(f"{final_directory}/GOOD_IMAGES")
+                            try:
+                                shutil.move(f"{UPLOAD_FOLDER}/{key}" , f"{final_directory}/GOOD_IMAGES")
+                                resultDisplayed["GOOD_IMAGES"] = 1
+                            except Exception as e:
+                                error += 1
+                            currentProgress +=1
+
+
+                    elif value == 4:
+                        #INVALID_PASSPORT
+                        if os.path.exists(f"{final_directory}/INVALID_PASSPORT") == True:
+                            try:
+                                shutil.move(f"{UPLOAD_FOLDER}/{key}" , f"{final_directory}/INVALID_PASSPORT")
+                                resultDisplayed["INVALID_PASSPORT"] += 1
+                            except:
+                                error += 1
+                        else:
+                            #make directory
+                            os.mkdir(f"{final_directory}/INVALID_PASSPORT")
+                            try:
+                                shutil.move(f"{UPLOAD_FOLDER}/{key}" , f"{final_directory}/INVALID_PASSPORT")
+                                resultDisplayed["INVALID_PASSPORT"] = 1
+                            except Exception as e:
+                                error += 1
+                            currentProgress +=1
+
+
+
+                    elif value == 5:
+                        #NO_HUMAN_FACE
+                        if os.path.exists(f"{final_directory}/NO_HUMAN_FACE") == True:
+                            try:
+                                shutil.move(f"{UPLOAD_FOLDER}/{key}" , f"{final_directory}/NO_HUMAN_FACE")
+                                resultDisplayed["NO_HUMAN_FACE"] += 1
+                            except:
+                                error += 1
+                        else:
+                            #make directory
+                            os.mkdir(f"{final_directory}/NO_HUMAN_FACE")
+                            try:
+                                shutil.move(f"{UPLOAD_FOLDER}/{key}" , f"{final_directory}/NO_HUMAN_FACE")
+                                resultDisplayed["NO_HUMAN_FACE"] = 1
+                            except Exception as e:
+                                error += 1
+                            currentProgress +=1
+
+
+                    elif value == 6:
+                        #STAPLED_FACE
+                        if os.path.exists(f"{final_directory}/STAPLED_FACE") == True:
+                            try:
+                                shutil.move(f"{UPLOAD_FOLDER}/{key}" , f"{final_directory}/STAPLED_FACE")
+                                resultDisplayed["STAPLED_FACE"] += 1
+                            except:
+                                error += 1
+                        else:
+                            #make directory
+                            os.mkdir(f"{final_directory}/STAPLED_FACE")
+                            try:
+                                shutil.move(f"{UPLOAD_FOLDER}/{key}" , f"{final_directory}/STAPLED_FACE")
+                                resultDisplayed["STAPLED_FACE"] = 1
+                            except Exception as e:
+                                error += 1
+                            currentProgress +=1
+                                    
+                #make zip file
+                shutil.make_archive(f'result_{today}', 'zip', final_directory)
+
+                #upload to firebase
+                firebase = pyrebase.initialize_app(config)
+                storage = firebase.storage()
+                path_on_cloud = f'results/{today}.zip'
+                storage.child(path_on_cloud).put(f'{final_directory}.zip')
+
+                upload_url = storage.child(path_on_cloud).get_url(token=None)
+
+
+
+                resultDisplayed["downloadLink"] = upload_url
+
+                print(f'result directory {upload_url}')
+                print(result)
+
+
+
+                 #remove image from local directory
+                shutil.rmtree(final_directory)
+                os.remove(f'{final_directory}.zip') 
+
+                return render_template("multipleFolderImagesPopUp.html", data=resultDisplayed)
+    else:
+        return "Use a POST request to access this API, check documentation at "
+            
+
+
+
+def saveFolder(uploaded_files):
+    cwd = os.getcwd()
+    config = {"apiKey": "AIzaSyC1ayNr3CCXRv-cejofLx1_hNmsR-o7Coo",
+                "authDomain": "passport-image-classification.firebaseapp.com",
+                "databaseURL": "https://passport-image-classification.firebaseio.com",
+                "projectId": "passport-image-classification",
+                "storageBucket": "passport-image-classification.appspot.com",
+                "messagingSenderId": "39733434768",
+                "appId": "1:39733434768:web:85ec9477120baa9116fb15",
+                "measurementId": "G-FXCMDZNL6T"}
+
+
+    for folder in uploaded_files:
+        if type(folder) == list:
+            saveFolder(folder)
+        else:
+            path = os.path.join('./test/test', folder.filename)
+            folder_path  = f'{folder.filename.split("/")[:-1]}'
+            folder_path = folder_path[2:-2]
+            folder_path = folder_path.replace("\', \'", "/")
+            filePath = f"{cwd}/test/test/{folder_path}" 
+            if os.path.exists(filePath) == True:
+                folder.save(f'{filePath}/{folder.filename.split("/")[-1]}')
+            else:
+                os.mkdir(filePath)
+                folder.save(f'{filePath}/{folder.filename.split("/")[-1]}')
+
+            #save to firebase
+            today = datetime.now()
+
+            firebase = pyrebase.initialize_app(config)
+            storage = firebase.storage()
+            path_on_cloud = f'images/{today}.jpg'
+            storage.child(path_on_cloud).put(f'{filePath}/{folder.filename.split("/")[-1]}')
+
+            upload_url = storage.child(path_on_cloud).get_url(token=None)
+
+
+            
+
+
+    '''
+    UPLOAD_FOLDER = './test/test'
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    for folder in uploaded_files:
+        if folder == []:
+            path = os.path.join(app.config['UPLOAD_FOLDER'], folder.filename)
+            file.save(path)
+        else:
+            saveFolder(folder)
+    
+    '''
 
         
         
